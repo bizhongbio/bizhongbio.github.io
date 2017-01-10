e62849c2e68db0638f5fc0bce5deabf918e673d6
bizhong <bizhonglan@gmail.com>
2017-01-10 21:27:33 +0800

    Slides add keydown & touch event

--- asset/js/bizhongbio.js
+++ asset/js/bizhongbio.js
@@ -6,12 +6,23 @@ $(function() {
       handleHome();
       break;
     default:// 匹配不存在
-      console.log('该页面暂无 JavaScript 操作！');
+      console.log('Nothing to do.');
   }
+
+  // ------------------------------------------------------------------ 公共
   
-  // 手机端导航的显示与隐藏
+  // 手机端点击汉堡菜单链接，导航菜单显示与隐藏
   $('#btn-nav').on('click', function() {
     $('#header, #cat-header').toggleClass('show-nav');
+    if ($('#header, #cat-header').hasClass('show-nav')) {
+      $('#btn-nav i').removeClass('fa-navicon').addClass('fa-remove');
+      $('body, html').on('touchmove', function(event) {
+        event.preventDefault();
+      });
+    } else {
+      $('#btn-nav i').removeClass('fa-remove').addClass('fa-navicon');
+      $('body, html').off('touchmove');
+    }
   });
 
   // 返回顶部 当滚动条的位置处于距顶部 600 像素以上时，返回顶部链接出现，否则隐藏
@@ -29,13 +40,18 @@ $(function() {
     event.preventDefault();
   });
 
+  // ------------------------------------------------------------------ 首页
+
   // 首页处理主函数
   function handleHome() {
-    var len = $('#hero-slides .slide').length,
-        slideIndex = 1,
-        timer;
+    var len = $('#hero-slides .slide').length,// 轮播图张数
+        slideIndex = 1,// 当前轮播图索引
+        timer,// 定时器
+        startPosition = {},// 开始位置
+        endPosition = {},// 结束位置
+        isScrolling = -1; // 判断是水平滚动还是垂直滚动
 
-    // 首页 轮播图切换
+    // 轮播图自动切换
     function slidesAnimation() {
       $('#hero-slides .slide').each(function(i) {
         $(this).eq(slideIndex).css('left', '0%').end().not(slideIndex).css('left', (-(slideIndex - i) * 100) + '%');
@@ -47,7 +63,7 @@ $(function() {
 
     timer = setInterval(slidesAnimation, 6000);
 
-    // 首页 点击轮播图导航切换
+    // 点击轮播图导航菜单，切换轮播图
     $('.slides-nav a').each(function() {
       $(this).on('click', function(event) {
         clearInterval(timer);
@@ -55,6 +71,58 @@ $(function() {
         slidesAnimation();
         event.preventDefault();
       });
+    });
+
+    // 按下键盘左右方向键，切换轮播图
+    $(document).on('keydown', function(event) {
+      if (event.keyCode === 37) {// 左方向键
+        clearInterval(timer);
+        slideIndex >= 2 ? slideIndex -= 2 : slideIndex += 2;
+        slidesAnimation();
+      } else if (event.keyCode === 39) {// 右方向键
+        clearInterval(timer);
+        slidesAnimation();
+      }
+    });
+
+    // 触屏设备左右滑动，切换轮播图
+
+    // 滑动开始
+    $('#hero-slides').on('touchstart', function(event) {
+      var touch = event.targetTouches[0];
+      startPosition = {
+        x: touch.pageX,
+        y: touch.pageY
+      };
+    });
+
+    // 滑动
+    $('#hero-slides').on('touchmove', function(event) {
+      if (event.targetTouches.length === 1) {
+        var touch = event.targetTouches[0];
+        endPosition = {
+          x: touch.pageX - startPosition.x,
+          y: touch.pageY - startPosition.y
+        };
+        isScrolling = Math.abs(endPosition.x) > Math.abs(endPosition.y) ? 0 : 1;// 0：水平滚动，1：垂直滚动
+        if (isScrolling === 0) {// 水平滚动
+          event.preventDefault();
+        }
+      }
+    });
+    
+    // 滑动释放
+    $('#hero-slides').on('touchend', function(event) {
+      if (isScrolling === 0) {// 水平滚动
+        if (endPosition.x > 10) {// 右滑
+          clearInterval(timer);
+          slideIndex >= 2 ? slideIndex -= 2 : slideIndex += 2;
+          slidesAnimation();
+        } else if (endPosition.x < -10) {// 左滑
+          clearInterval(timer);
+          slidesAnimation();
+        }
+      }
     });
   }
 });
