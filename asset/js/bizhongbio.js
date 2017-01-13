4ace752d9a34adf123f5ad4a747087d5145a07b9
bizhong <bizhonglan@gmail.com>
2017-01-14 00:13:10 +0800

    Add work page

--- asset/js/bizhongbio.js
+++ asset/js/bizhongbio.js
@@ -7,6 +7,16 @@ $(function() {
       break;
     case 'category':// 分类目录页面
       handleArticle();
+      catHeaderFixed();
+      break;
+    case 'page-work':// 作品页面
+      handleWork();
+
+      // 当调整浏览器窗口的大小时，重新作品页面处理主函数
+      $(window).on('resize', function() {
+        handleWork();
+      });
+      break;
     default:// 匹配不存在
       console.log('Nothing to do.');
   }
@@ -16,6 +26,7 @@ $(function() {
   // 手机端点击汉堡菜单链接，导航菜单显示与隐藏
   $('#btn-nav').on('click', function() {
     $('#header, #cat-header').toggleClass('show-nav');
+
     if ($('#header, #cat-header').hasClass('show-nav')) {
       $('#btn-nav i').removeClass('fa-navicon').addClass('fa-remove');
       $('body, html').on('touchmove', function(event) {
@@ -61,7 +72,7 @@ $(function() {
 
   // 返回顶部 点击返回顶部链接，返回顶部
   $('#go-top').on('click', function(event) {
-    $('body, html').animate({scrollTop: 0}, 600);
+    $('body, html').animate({'scrollTop': '0'}, 600);
     event.preventDefault();
   });
 
@@ -100,13 +111,16 @@ $(function() {
 
     // 按下键盘左右方向键，切换轮播图
     $(document).on('keydown', function(event) {
-      if (event.keyCode === 37) {// 左方向键
-        clearInterval(timer);
-        slideIndex >= 2 ? slideIndex -= 2 : slideIndex += 2;
-        slidesAnimation();
-      } else if (event.keyCode === 39) {// 右方向键
-        clearInterval(timer);
-        slidesAnimation();
+      switch(event.keyCode) {
+        case 37:// 左方向键
+          clearInterval(timer);
+          slideIndex >= 2 ? slideIndex -= 2 : slideIndex += 2;
+          slidesAnimation();
+          break;
+        case 39:// 右方向键
+          clearInterval(timer);
+          slidesAnimation();
+          break;
       }
     });
 
@@ -130,6 +144,7 @@ $(function() {
           y: touch.pageY - startPosition.y
         };
         isScrolling = Math.abs(endPosition.x) > Math.abs(endPosition.y) ? 0 : 1;// 0：水平滚动，1：垂直滚动
+
         if (isScrolling === 0) {// 水平滚动
           event.preventDefault();
         }
@@ -151,9 +166,9 @@ $(function() {
     });
   }
 
-  // ------------------------------------------------------------------ 分类目录
+  // ------------------------------------------------------------------ 分类目录页面
 
-  // 分类目录处理主函数
+  // 分类目录页面处理主函数
   function handleArticle() {
 
     // 点击分类目录导航链接，显示对应分类目录文章
@@ -171,7 +186,65 @@ $(function() {
         }
       });
     });
+  }
 
-    catHeaderFixed();
+  // ------------------------------------------------------------------ 作品页面
+
+  // 作品页面处理主函数
+  function handleWork() {
+    var workWidth = $('#works .work').width(),// 单个作品宽度
+        columns = Math.floor($('#works').width() / workWidth),// 作品列数
+        columnsHeight = [];// 作品列高
+    
+    // 每个作品定位
+    $('#works .work').each(function(i) {
+      var columnIndex = 0;// 作品列索引
+      if (i < columns) {// 作品第一行
+        columnsHeight[i] = $(this).height();// 当前作品图片高度写进数组
+        $(this).css({
+          'position': 'absolute',
+          'top': '0px',
+          'left': i * workWidth + 'px'
+        });
+      } else {// 作品其它行
+        var columnMinHeight = Math.min.apply(null, columnsHeight);// 作品图片最小列高
+        $.each(columnsHeight, function(j) {
+          if (columnsHeight[j] === columnMinHeight) {
+            columnIndex = j;
+          }
+        });
+
+        $(this).css({
+          'position': 'absolute',
+          'top': columnMinHeight + 'px',
+          'left': columnIndex * workWidth + 'px'
+        });
+        columnsHeight[columnIndex] += $(this).height();
+      }
+    });
+
+    $('#works').css({'height': Math.max.apply(null, columnsHeight) + 'px'});
+
+    // 作品点击放大图片
+    $('#works .work').on('click', function(event) {
+      var self = $(this);// 当前点击的作品对象
+
+      // 当前点击的作品有 work-current 类，不可再次点击
+      if (!self.hasClass('work-current')) {
+        var workLeft = self.css('left');// 当前点击的作品距离左边的距离
+
+        $('#mask').css('display', 'block');
+        $('body, html').scrollTop(self.offset().top);// 滚动条滚动到当前点击的作品距离文档的上方位置
+
+        $(this).addClass('work-current').siblings().removeClass('work-current');
+        $(this).css('left', '50%');
+      }
+
+      // 蒙版点击退出
+      $('#mask').on('click', function() {
+        self.removeClass('work-current').css('left', workLeft);
+        $('#mask').css('display', 'none');
+      });
+    });
   }
 });
